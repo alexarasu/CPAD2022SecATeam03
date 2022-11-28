86ececd3e666d52ae70d182f5830fdf554b74f08
Ezhil Arasu <alex.ezhil.arasu@sap.com>
2022-11-28 21:54:44 +0530

    Firebase setup for Google Authentication

--- techogram/lib/main.dart
+++ techogram/lib/main.dart
@@ -1,9 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:techogram/features/auth/screens/login_screen.dart';
 import 'package:techogram/theme/pallete.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
+  // runApp(
+  //   const ProviderScope(
+  //     child: MyApp(),
+  //   ),
+  // );
 }
 
 class MyApp extends StatelessWidget {
