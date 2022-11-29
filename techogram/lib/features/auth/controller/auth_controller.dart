b9f6c822ce122cf5571680d08a6e722a2292b8f5
Hazel Shefali John <hazelshefalijohn@gmail.com>
2022-11-29 17:22:58 +0530

    data storage and exception handling

--- techogram/lib/features/auth/controller/auth_controller.dart
+++ techogram/lib/features/auth/controller/auth_controller.dart
@@ -1,19 +1,35 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:techogram/core/utils.dart';
 import 'package:techogram/features/auth/repository/auth_repository.dart';
 
-final authControllerProvider = Provider(
+import '../../../models/user_model.dart';
+
+final userProvider = StateProvider<UserModel?>((ref) => null);
+
+final authControllerProvider = StateNotifierProvider<AuthController, bool>(
   (ref) => AuthController(
-    authRepository: ref.read(authRepositoryProvider),
+    authRepository: ref.watch(authRepositoryProvider),
+    ref: ref,
   ),
 );
 
-class AuthController {
+class AuthController extends StateNotifier<bool> {
   final AuthRepository _authRepository;
-  AuthController({required AuthRepository authRepository})
-      : _authRepository = authRepository;
+  final Ref _ref;
+  AuthController({required AuthRepository authRepository, required Ref ref})
+      : _authRepository = authRepository,
+        _ref = ref,
+        super(false); //represents loading
 
-  void signInWithGoogle() {
-    _authRepository.signInWithGoogle();
+  void signInWithGoogle(BuildContext context) async {
+    state = true;
+    final user = await _authRepository.signInWithGoogle();
+    state = false; //after fetching value of user
+    user.fold(
+        (l) => showSnackBar(context, l.message),
+        (UserModel) =>
+            _ref.read(userProvider.notifier).update((state) => UserModel));
   }
 
   void logout() async {
--- techogram/lib/features/auth/screens/login_screen.dart
+++ techogram/lib/features/auth/screens/login_screen.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:techogram/core/common/loader.dart';
 import 'package:techogram/core/common/sign_in_button.dart';
 import 'package:techogram/core/constants/constants.dart';
+import 'package:techogram/features/auth/controller/auth_controller.dart';
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends ConsumerWidget {
   const LoginScreen({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final isLoading = ref.watch(authControllerProvider);
     return Scaffold(
       appBar: AppBar(
         title: Image.asset(
@@ -29,29 +33,31 @@ class LoginScreen extends StatelessWidget {
       // body: isLoading
       //     ? const Loader()
       //     : Column(
-      body: Column(
-        children: [
-          const SizedBox(height: 30),
-          const Text(
-            'Your Gram for Tech',
-            style: TextStyle(
-              fontSize: 24,
-              fontWeight: FontWeight.bold,
-              letterSpacing: 0.5,
+      body: isLoading
+          ? const Loader()
+          : Column(
+              children: [
+                const SizedBox(height: 30),
+                const Text(
+                  'Your Gram for Tech',
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 0.5,
+                  ),
+                ),
+                const SizedBox(height: 30),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Image.asset(
+                    Constants.loginEmotePath,
+                    height: 400,
+                  ),
+                ),
+                const SizedBox(height: 20),
+                const SignInButton(),
+              ],
             ),
-          ),
-          const SizedBox(height: 30),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Image.asset(
-              Constants.loginEmotePath,
-              height: 400,
-            ),
-          ),
-          const SizedBox(height: 20),
-          const SignInButton(),
-        ],
-      ),
     );
   }
 }
