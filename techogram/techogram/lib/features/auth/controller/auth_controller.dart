4ad3abff7101df5bc5dca54c52e56ab1193e7478
Ezhil Arasu <alex.ezhil.arasu@sap.com>
2022-12-01 12:18:24 +0530

    Added file changes for OSX login issue

---
+++ techogram/techogram/lib/features/auth/controller/auth_controller.dart
@@ -0,0 +1,55 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:techogram/core/utils.dart';
+import 'package:techogram/features/auth/repository/auth_repository.dart';
+
+import '../../../models/user_model.dart';
+
+final userProvider = StateProvider<UserModel?>((ref) => null);
+
+final authControllerProvider = StateNotifierProvider<AuthController, bool>(
+  (ref) => AuthController(
+    authRepository: ref.watch(authRepositoryProvider),
+    ref: ref,
+  ),
+);
+
+final authStateChangeProvider = StreamProvider((ref) {
+  final authController = ref.watch(authControllerProvider.notifier);
+  return authController.authStateChange;
+});
+
+final getaUserDataProvider = StreamProvider.family((ref, String uid) {
+  final authController = ref.watch(authControllerProvider.notifier);
+  return authController.getUserData(uid);
+});
+
+class AuthController extends StateNotifier<bool> {
+  final AuthRepository _authRepository;
+  final Ref _ref;
+  AuthController({required AuthRepository authRepository, required Ref ref})
+      : _authRepository = authRepository,
+        _ref = ref,
+        super(false); //represents loading
+
+  Stream<User?> get authStateChange => _authRepository.authStateChange;
+
+  void signInWithGoogle(BuildContext context) async {
+    state = true;
+    final user = await _authRepository.signInWithGoogle();
+    state = false; //after fetching value of user
+    user.fold(
+        (l) => showSnackBar(context, l.message),
+        (userModel) =>
+            _ref.read(userProvider.notifier).update((state) => userModel));
+  }
+
+  Stream<UserModel> getUserData(String uid) {
+    return _authRepository.getUserData(uid);
+  }
+
+  void logout() async {
+    _authRepository.logOut();
+  }
+}
